a88dbf360b2790cdf70565700308a172824cbb7b
Jingchen Xu <joel.jc.xu@gmail.com>
2016-01-07 23:35:46 -0500

    Migrate files from private repository
    Since the Platypus Commons repository was becoming somewhat unweildy,
    move the PUtils project into its own repo for modularity's sake. Also
    add a bit of polish in terms of style and documentation to keep things
    looking reasonably presentable.

---
+++ Platypus Utils/src/platypus/util/general/PFileUtils.java
@@ -0,0 +1,268 @@
+package platypus.util.general;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A collection of methods for manipulating files.
+ * 
+ * @author Jingchen Xu
+ */
+public class PFileUtils {
+
+	private PFileUtils() {}
+
+	/**
+	 * Recursively lists all children of a specified directory. If the specified
+	 * directory is empty, an empty list will be returned. If the argument is
+	 * not a directory, a list containing only that file will be returned.
+	 * 
+	 * @param file the directory to be searched
+	 * @return a list of all files in the directory, including sub-directories
+	 */
+	public static ArrayList<File> deepListFiles(File file) {
+
+		ArrayList<File> fileList = new ArrayList<File>();
+
+		if (file.isDirectory()) {
+			File[] children = file.listFiles();
+			for (File child : children) {
+				fileList.addAll(deepListFiles(child));
+			}
+		} else {
+			fileList.add(file);
+		}
+
+		return fileList;
+	}
+
+	/**
+	 * Recursively lists all children of a specified directory matching the
+	 * specified filter. If the specified directory contains no such files, an
+	 * empty list will be returned. If the argument is not a directory but
+	 * matches the filter, a list containing only that file will be returned.
+	 * 
+	 * @param file the directory to be searched
+	 * @param filter the filter to apply on the search
+	 * @return a list of all files matching the filter in the directory,
+	 *         including sub-directories
+	 */
+	public static ArrayList<File> deepListFiles(File file,
+			FileFilter filter) {
+
+		ArrayList<File> fileList = new ArrayList<File>();
+
+		if (file.isDirectory()) {
+			File[] children = file.listFiles();
+			for (File child : children) {
+				fileList.addAll(deepListFiles(child));
+			}
+		} else if (filter.accept(file)) {
+			fileList.add(file);
+		}
+
+		return fileList;
+	}
+
+	/**
+	 * Sets the value of a user defined file attribute for a specified file. If
+	 * a previous value exists, it is overwritten and that value is returned.
+	 * 
+	 * @param file the file to add the attribute to
+	 * @param attribute the name of the attribute
+	 * @param value the value of the attribute. If null, the attribute will be
+	 *            deleted instead.
+	 * @return the previous value of the attribute. If no such value existed,
+	 *         null is returned
+	 */
+	public static String setUserDefinedFileAttribute(File file,
+			String attribute, String value) {
+
+		if (!file.exists())
+			throw new IllegalArgumentException("Specified file does not exist");
+
+		Path path = Paths.get(file.getAbsolutePath());
+		UserDefinedFileAttributeView view = Files.getFileAttributeView(path,
+				UserDefinedFileAttributeView.class);
+
+		// read previous value
+		try {
+			String previousValue = null;
+			if (view.size(attribute) > 0) {
+				ByteBuffer buf = ByteBuffer.allocate(view.size(attribute));
+				view.read(attribute, buf);
+				buf.flip();
+				previousValue = Charset.defaultCharset().decode(buf).toString();
+
+				System.out.printf("Replaced previous value: %s\n",
+						previousValue);
+			}
+
+			return previousValue;
+		} catch (NoSuchFileException e) {
+			// no previous value
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			// write new value
+			try {
+				if (value == null)
+					view.delete(attribute);
+				else
+					view.write(attribute,
+							Charset.defaultCharset().encode(value));
+
+			} catch (NoSuchFileException e) {
+				System.err.println("Attribute not deleted; " + attribute
+						+ " not found");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Batch-sets multiple user defined file attribute on a specified file.
+	 * Behaves in the same way as setUserDefinedFileAttribute().
+	 * 
+	 * @param file the file to add attributes to
+	 * @param attributes a map containing all the attribute-value pairs to be
+	 *            written
+	 * @return
+	 */
+	public static Map<String, String> setUserDefinedFileAttributes(
+			File file, Map<String, String> attributes) {
+
+		Map<String, String> previousVals = new HashMap<String, String>();
+
+		for (String key : attributes.keySet()) {
+			String previousVal = setUserDefinedFileAttribute(file, key,
+					attributes.get(key));
+			previousVals.put(key, previousVal);
+		}
+
+		return previousVals;
+	}
+
+	/**
+	 * Reads the value of a specified user defined file attribute from a file.
+	 * 
+	 * @param file the file to read the attribute from
+	 * @param attribute the attribute to read
+	 * @return the value of that attribute
+	 */
+	public static String readUserDefinedFileAttribute(File file,
+			String attribute) {
+
+		if (!file.exists())
+			return null;
+
+		Path path = Paths.get(file.getAbsolutePath());
+		UserDefinedFileAttributeView view = Files.getFileAttributeView(path,
+				UserDefinedFileAttributeView.class);
+
+		try {
+			ByteBuffer buf = ByteBuffer.allocate(view.size(attribute));
+			view.read(attribute, buf);
+			buf.flip();
+			String value = Charset.defaultCharset().decode(buf).toString();
+
+			return value;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Reads all user defined file attributes attached to a file.
+	 * 
+	 * @param file the file to read attributes from
+	 * @return a map containing all the attribute-value pairs attached to the
+	 *         file
+	 */
+	public static Map<String, String> readUserDefinedFileAttributes(
+			File file) {
+
+		if (!file.exists())
+			return null;
+
+		Path path = Paths.get(file.getAbsolutePath());
+		UserDefinedFileAttributeView view = Files.getFileAttributeView(path,
+				UserDefinedFileAttributeView.class);
+
+		Map<String, String> output = new HashMap<String, String>();
+		try {
+			for (String attribute : view.list()) {
+				// read each attribute
+				ByteBuffer buf = ByteBuffer.allocate(view.size(attribute));
+				view.read(attribute, buf);
+				buf.flip();
+				String value = Charset.defaultCharset().decode(buf).toString();
+
+				// add to the map
+				output.put(attribute, value);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return output;
+	}
+
+	/**
+	 * Deletes a specified user defined file attribute from a file.
+	 * 
+	 * @param file the file to delete the attribute from
+	 * @param attribute the attribute to delete
+	 */
+	public static void clearUserDefinedFileAttribute(File file, String attribute) {
+		if (!file.exists())
+			return;
+
+		Path path = Paths.get(file.getAbsolutePath());
+		UserDefinedFileAttributeView view = Files.getFileAttributeView(path,
+				UserDefinedFileAttributeView.class);
+		try {
+			view.delete(attribute);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Deletes all user defined file attributes attached to a file.
+	 * 
+	 * @param file the file to delete attributes from
+	 */
+	public static void clearUserDefinedFileAttributes(File file) {
+		if (!file.exists())
+			return;
+
+		Path path = Paths.get(file.getAbsolutePath());
+		UserDefinedFileAttributeView view = Files.getFileAttributeView(path,
+				UserDefinedFileAttributeView.class);
+		try {
+			for (String attribute : view.list()) {
+				view.delete(attribute);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Platypus Utils/src/platypus/util/monitoring/ClipboardMonitor.java
@@ -0,0 +1,147 @@
+package platypus.util.monitoring;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+/**
+ * A <code>ClipboardMonitor</code> is a <code>ClipboardOwner</code> which
+ * detects changes in the system clipboard and sends notifications through an
+ * ActionListener interface. String-flavor clipboard contents are sent as
+ * commands in their corresponding <code>ActionEvent</code>s.
+ * <p>
+ * The <code>ClipboardMonitor</code> operates on its own thread, which must be
+ * started manually. Additionally, the <code>resumeListening()</code> must be
+ * called for the <code>ClipboardMonitor</code> to begin detecting changes,
+ * since this is turned off by default.
+ * <p>
+ * Irregular behavior can occur if multiple <code>ClipboardMonitor</code>
+ * threads run concurrently.
+ * 
+ * @author Jingchen Xu
+ */
+public class ClipboardMonitor extends Thread implements ClipboardOwner {
+
+	private Clipboard sysClip = Toolkit.getDefaultToolkit()
+			.getSystemClipboard();
+	private boolean end = false;
+	private boolean listening = false;
+	private String prevVal = "";
+
+	private ArrayList<ActionListener> listeners = new ArrayList<ActionListener>();
+
+	// TODO change to singleton
+
+	public ClipboardMonitor() {
+		super();
+	}
+
+	@Override
+	public void run() {
+		Transferable trans = sysClip.getContents(this);
+		regainOwnership(trans);
+		// System.out.println("Listening to board...");
+		while (true) {
+			if (isOver())
+				break;
+		}
+		// System.out.println("No more Listening...");
+	}
+
+	/**
+	 * Allows the listener to begin detecting clipboard changes. By default,
+	 * listening is turned off.
+	 */
+	public void resumeListening() {
+		listening = true;
+	}
+
+	/**
+	 * Prevents the listener from detecting clipboard changes.
+	 */
+	public void pauseListening() {
+		listening = false;
+	}
+
+	/**
+	 * Signals the thread to join.
+	 */
+	public void end() {
+		end = true;
+	}
+
+	private boolean isOver() {
+		return end;
+	}
+
+	@Override
+	public void lostOwnership(Clipboard c, Transferable t) {
+		try {
+			sleep(200);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+		try {
+			Transferable contents = c.getContents(this); // EXCEPTION
+			processContents(contents);
+			regainOwnership(contents);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void processContents(Transferable t) {
+
+		if (!listening)
+			return;
+
+		try {
+			Object o = t.getTransferData(DataFlavor.stringFlavor);
+			if (!o.equals(prevVal)) {
+				notifyListeners((String) o);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void regainOwnership(Transferable t) {
+		sysClip.setContents(t, this);
+		processContents(t);
+	}
+
+	/**
+	 * Adds an <code>ActionListener</code> to the monitor.
+	 * 
+	 * @param l
+	 *            the <code>ActionListener</code> to be added
+	 */
+	public void addActionListener(ActionListener l) {
+		listeners.add(l);
+	}
+
+	/**
+	 * Removes an <code>ActionListener</code> to the monitor.
+	 * 
+	 * @param l
+	 *            the <code>ActionListener</code> to be removed
+	 */
+	public void removeActionListener(ActionListener l) {
+		listeners.remove(l);
+	}
+
+	private void notifyListeners(String s) {
+
+		synchronized (listeners) {
+			for (int i = 0; i < listeners.size(); i++)
+				listeners.get(i).actionPerformed(
+						new ActionEvent(this, ActionEvent.ACTION_PERFORMED, s));
+			prevVal = s;
+		}
+	}
+}
